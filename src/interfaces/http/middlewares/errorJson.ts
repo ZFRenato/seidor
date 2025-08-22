3292eefbe0df882f3ed5c39177f1e5bce46b9d28
Renato <renatopscarlos@gmail.com>
2025-08-22 15:29:15 -0300

    refactor: fit errorJson

--- src/interfaces/http/middlewares/errorJson.ts
+++ src/interfaces/http/middlewares/errorJson.ts
@@ -3,9 +3,7 @@ import { response } from '../util/responseHttp';
 
 export const errorJson = (err: Error, _req: Request, res: Response, next: NextFunction) => {
   if (err instanceof SyntaxError && 'body' in err) {
-	const result = response.badRequest({ message: err.message });
-    return res.status(result.statusCode).json(result.body);
+	response.badRequest(res, { message: err.message });
   }
-
   next();
 };
